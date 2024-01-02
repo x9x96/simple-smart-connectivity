1f8e9d1862a0fe69cc316ffe2098dc2eb623c175
EseVic <esevic111@gmail.com>
2024-01-02 23:33:37 +0100

    Task: Written RangeRegulator function

--- SmartHome.kt
+++ SmartHome.kt
@@ -197,3 +197,25 @@ class SmartHome(
     }
 }
 
+class RangeRegulator(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int
+) : ReadWriteProperty<Any?, Int> {
+
+    var fieldData = initialValue
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            fieldData = value
+        }
+    }
+}
+
+fun main() {    
+
+}
