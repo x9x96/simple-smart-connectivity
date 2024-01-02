865eebdd365920f3442d64cc947e8b7232c4049e
EseVic <esevic111@gmail.com>
2024-01-02 23:30:18 +0100

    Task: Written Smart Light Device function

--- SmartHome.kt
+++ SmartHome.kt
@@ -72,3 +72,37 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
 	    super.printDeviceInfo()
     }
 }
+
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart Light"
+
+    private var brightnessLevel by RangeRegulator(initialValue = 10, minValue = 2, maxValue = 100)
+
+    override fun turnOn() {
+        super.turnOn()
+        brightnessLevel = 10
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+
+    fun decreaseBrightness() {
+        brightnessLevel--
+        println("Brightness decreased to $brightnessLevel.")
+    }    
+        
+    fun printLightInfo() {
+        super.printDeviceInfo()
+    }
+}
