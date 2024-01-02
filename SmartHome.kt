68159cf5da8e7ea37f136110b9f8943c37123893
EseVic <esevic111@gmail.com>
2024-01-02 23:36:19 +0100

    Task: Fixed up codebase

--- SmartHome.kt
+++ SmartHome.kt
@@ -1,10 +1,22 @@
 /*
  * Copyright (C) 2024 Simple Android Smart Control
+ * by: Israel Oyetunji
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 open class SmartDevice(val name: String, val category: String) {
 
@@ -215,6 +227,7 @@ class RangeRegulator(
         }
     }
 }
+
 fun main() {    
 	var smartHome = SmartHome(SmartTvDevice("Android TV", "Entertainment"), SmartLightDevice("Google Light", "Utility"))
     
